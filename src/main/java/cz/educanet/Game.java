01261f31376e36fb5bd7045d724a5723110d864e
unknown <oliver.cavallo@educanet.cz>
2021-04-26 21:16:29 +0200

    update

--- src/main/java/cz/educanet/Game.java
+++ src/main/java/cz/educanet/Game.java
@@ -1,5 +1,7 @@
 package cz.educanet;
 
+import cz.educanet.models.Square;
+import cz.educanet.utils.FileUtils;
 import org.joml.Matrix4f;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.glfw.GLFW;
@@ -8,6 +10,7 @@ import org.lwjgl.system.MemoryUtil;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
+import java.util.ArrayList;
 
 public class Game {
     private static final float[] vertices = {
@@ -41,7 +44,32 @@ public class Game {
     // 4x4 -> FloatBuffer of size 16
     private static FloatBuffer matrixFloatBuffer = BufferUtils.createFloatBuffer(16);
 
+
+    private static ArrayList<Square> square = new ArrayList<Square>();
+    private static String[] maze;
+    private static float mazeRow;
+    private static float mazeCol;
+
+
+
     public static void init(long window) {
+
+
+        Shaders.initShaders();
+        maze = FileUtils.readFile("src/main/java/cz/educanet/resources/1lvl.txt").split("\n");
+
+        mazeRow = 2.0f / maze[0].length();
+        mazeCol = 2.0f / maze.length;
+
+
+        for (int y = 0;y < maze.length;y++){
+            String line = maze[y];
+            for(int x = 0;x < line.length();x++){
+                if (line.charAt(x) == '0') {
+                    square.add(new Square((x * mazeRow) * (+1) - 1 ,(y * mazeCol) * (-1) + 1, mazeRow, mazeCol));
+                }
+            }
+        }
         // Setup shaders
         Shaders.initShaders();
 
@@ -106,6 +134,11 @@ public class Game {
 
     public static void render(long window) {
 
+        GL33.glUseProgram(Shaders.shaderProgramId);
+
+        for (Square square : square){
+            square.draw();
+        }
         // Draw using the glDrawElements function
         GL33.glBindVertexArray(squareVaoId);
         GL33.glDrawElements(GL33.GL_TRIANGLES, indices.length, GL33.GL_UNSIGNED_INT, 0);
---
+++ src/main/java/cz/educanet/models/Square.java
@@ -0,0 +1,99 @@
+package cz.educanet.models;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL33;
+import org.lwjgl.system.MemoryUtil;
+
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+public class Square {
+
+
+    private static final int[] indices = {
+            0, 1, 2, // First triangle
+            1, 2, 3, // Second triangle
+
+    };
+
+    private float[] colors = {
+            1.0f, 1.0f, 1.0f,
+            1.0f, 1.0f, 1.0f,
+            1.0f, 1.0f, 1.0f,
+            1.0f, 1.0f, 1.0f,
+    };
+
+    private int squareVaoId;
+    private int squareVboId;
+    private int squareEboId;
+    private int colorsId;
+
+    private FloatBuffer cb;
+
+    public Square(float x, float y, float width, float height) {
+        float[] vertices = {
+                -1.0f , 1.0f , 1.0f,
+                -1.0f , 1.0f , 1.0f,
+                -1.0f , 1.0f , 1.0f,
+                -1.0f , 1.0f , 1.0f,
+        };
+        squareVaoId = GL33.glGenVertexArrays();
+
+        squareVboId = GL33.glGenBuffers();
+        squareEboId = GL33.glGenBuffers();
+        colorsId = GL33.glGenBuffers();
+
+        for (int i = 0; i < 4; i++) {
+            vertices[i * 3] = width * (i % 2) + x;
+            vertices[i * 3 + 1] = y - width * (Math.round(i / 2));
+        }
+
+        GL33.glBindVertexArray(squareVaoId);
+
+        GL33.glBindBuffer(GL33.GL_ELEMENT_ARRAY_BUFFER, squareEboId);
+        IntBuffer ib = BufferUtils.createIntBuffer(indices.length)
+                .put(indices)
+                .flip();
+        GL33.glBufferData(GL33.GL_ELEMENT_ARRAY_BUFFER, ib, GL33.GL_STATIC_DRAW);
+
+        GL33.glBindBuffer(GL33.GL_ARRAY_BUFFER, squareVboId);
+
+        FloatBuffer fb = BufferUtils.createFloatBuffer(vertices.length)
+                .put(vertices)
+                .flip();
+
+        GL33.glBufferData(GL33.GL_ARRAY_BUFFER, fb, GL33.GL_STATIC_DRAW);
+        GL33.glVertexAttribPointer(0, 3, GL33.GL_FLOAT, false, 0, 0);
+        GL33.glEnableVertexAttribArray(0);
+
+        MemoryUtil.memFree(fb);
+
+
+        GL33.glBindBuffer(GL33.GL_ARRAY_BUFFER, colorsId);
+
+        cb = BufferUtils.createFloatBuffer(colors.length)
+                .put(colors)
+                .flip();
+
+        GL33.glBufferData(GL33.GL_ARRAY_BUFFER, cb, GL33.GL_STATIC_DRAW);
+        GL33.glVertexAttribPointer(1, 3, GL33.GL_FLOAT, false, 0, 0);
+        GL33.glEnableVertexAttribArray(1);
+
+
+
+    }
+
+    public Square(float[] arr, int[] arr2, float[] arr1) {
+    }
+
+    public void draw() {
+        GL33.glBindVertexArray(squareVaoId);
+        GL33.glDrawElements(GL33.GL_TRIANGLES, indices.length, GL33.GL_UNSIGNED_INT, 0);
+
+        GL33.glBindBuffer(GL33.GL_ARRAY_BUFFER, colorsId);
+        cb.put(colors).flip();
+        GL33.glBufferData(GL33.GL_ARRAY_BUFFER, cb, GL33.GL_STATIC_DRAW);
+    }
+
+
+}
